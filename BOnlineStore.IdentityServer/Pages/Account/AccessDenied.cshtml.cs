6f1345b73a1c4048b1032ecda58777c21a83150d
scakir1978 <scakir1978@gmail.com>
2022-05-27 22:18:36 +0300

    Identity server ilk kurulum

---
+++ BOnlineStore.IdentityServer/Pages/Account/AccessDenied.cshtml.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BOnlineStore.IdentityServer.Pages.Account;
+
+public class AccessDeniedModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
